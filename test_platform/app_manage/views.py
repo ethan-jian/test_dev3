255651cdf69c6cba068a6b59276ff87cd3c5eb1a
ethan <729381317@qq.com>
2020-05-14 01:05:04 +0800

    实现项目创建1

--- test_platform/app_manage/views.py
+++ test_platform/app_manage/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from app_manage.models import Project
+from app_manage.forms import ProjectForm
 
 # Create your views here.
-
 
 @login_required
 def mange(request):
@@ -18,7 +19,19 @@ def add_project(request):
     """
     新增项目
     """
-    return render(request, 'project_add.html',)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            describe = form.cleaned_data['describe']
+            status = form.cleaned_data['status']
+            Project.objects.create(name=name, describe=describe, status=status)
+
+        return HttpResponseRedirect('/project/')
+    else:
+        form = ProjectForm()
+
+    return render(request, 'project_add.html', {'form': form})
 
 
 # def mange(requset):
